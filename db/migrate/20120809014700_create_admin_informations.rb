a4baf38d39ece6e3213d275c064f5f6062e29425
msj <464840999@qq.com>
2012-08-12 11:50:51 +0800

    update_code_style

--- db/migrate/20120809014700_create_admin_informations.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdminInformations < ActiveRecord::Migration
-  def change
-    create_table :admin_informations do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20120809014811_create_admin_information.rb
+++
@@ -1,8 +0,0 @@
-class CreateAdminInformation < ActiveRecord::Migration
-  def change
-    create_table :admin_information do |t|
-
-      t.timestamps
-    end
-  end
-end
